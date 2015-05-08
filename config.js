1fffabfaf8e78cfc97556be517a0b2645cb122f0
crissdev <criss.trifan@gmail.com>
2015-05-08 11:54:31 +0300

    update dependencies

--- config.js
+++ config.js
@@ -15,13 +15,13 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.2.16",
-    "babel-runtime": "npm:babel-runtime@5.2.16",
+    "babel": "npm:babel-core@5.2.17",
+    "babel-runtime": "npm:babel-runtime@5.2.17",
     "bootstrap": "github:twbs/bootstrap@3.3.4",
-    "core-js": "npm:core-js@0.9.6",
+    "core-js": "npm:core-js@0.9.7",
     "crossroads": "github:millermedeiros/crossroads.js@0.12.0",
     "hasher": "github:millermedeiros/hasher@1.2.0",
-    "jquery": "github:components/jquery@2.1.3",
+    "jquery": "github:components/jquery@2.1.4",
     "knockout": "github:knockout/knockout@3.3.0",
     "knockout-punches": "github:mbest/knockout.punches@0.5.1",
     "signals": "github:millermedeiros/js-signals@1.0.0",
@@ -31,9 +31,9 @@ System.config({
       "process": "npm:process@0.10.1"
     },
     "github:twbs/bootstrap@3.3.4": {
-      "jquery": "github:components/jquery@2.1.3"
+      "jquery": "github:components/jquery@2.1.4"
     },
-    "npm:core-js@0.9.6": {
+    "npm:core-js@0.9.7": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:sentence-case@1.1.2": {
